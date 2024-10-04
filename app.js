bfdaaba3d8b81fb6657504350185ed853565ef26
Luana Almeida <134015897+Luaalmed@users.noreply.github.com>
2024-10-04 14:49:40 +0000

    adicionando as imagens

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ function alterarStatus(id){
 
     let imagem = gameClicado.querySelector('.dashboard_item_img');
     let botao = gameClicado.querySelector('.dashboard_item_img');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    alert(nomeJogo.textContent);
 }
