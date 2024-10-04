1a7d0f5c0a82f1c2667dac6d6c209879e99fa16c
Luana Almeida <134015897+Luaalmed@users.noreply.github.com>
2024-10-04 15:26:07 +0000

    criando efeito no botao, ficar claro escuro

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
-
     let imagem = gameClicado.querySelector('.dashboard_item_img');
     let botao = gameClicado.querySelector('.dashboard_item_img');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-
-    alert(nomeJogo.textContent);
+    
+    if (imagem.classList.contains('dashboard_item_img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+    }
 }
